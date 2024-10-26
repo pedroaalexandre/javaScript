3e92fcbc11758b28072067d55b918471fed9bcb0
Pedro Augusto <162388916+pedroaalexandre@users.noreply.github.com>
2024-10-26 12:19:47 -0300

    Add files via upload

---
+++ senhaForte/js/scripts.js
@@ -0,0 +1,16 @@
+let letrasNumeros = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+];
+
+var senhaForte = "";
+
+var tamanhoSenha = prompt("Quantidade de dígitos da senha aleatória:")
+
+for(i = 0; i < tamanhoSenha; i++){
+    var senhaAleat = parseInt(Math.random()*63);
+    senhaForte += letrasNumeros[senhaAleat];
+}
+
+document.write(`<h1>Senha forte: ${senhaForte}</h1>`);
